9f5c1fc6cb7ad41810cc80cbe40c653805332fac
galperins4 <galperins4@gmail.com>
2018-03-18 22:34:37 -0500

    Update acedb.py

--- db/acedb.py
+++ db/acedb.py
@@ -32,8 +32,7 @@ class AceDB:
 
     def storeContracts(self, contracts):
         newContracts=[]
-	print(contracts)
-	print(contracts[2])
+	
 	for c in contracts:
             print(c)
             self.cursor.execute("SELECT contract FROM contracts WHERE contract = ?", (c[0],))
