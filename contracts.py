13d0caad35934c1e2aaf749da93a0d3357ca3315
galperins4 <galperins4@gmail.com>
2018-03-20 20:29:18 -0500

    Update contracts.py

--- contracts.py
+++ contracts.py
@@ -13,37 +13,37 @@ def parse_config():
     """
     with open('config/config.json') as data_file:
         data = json.load(data_file)
-        
+
     with open('config/networks.json') as network_file:
         network = json.load(network_file)
 
     return data, network
 
 if __name__ == '__main__':
-    
+
     # get config data
     data, network = parse_config()
-    
+
     # check to see if ark.db exists, if not initialize db, etc
-    if os.path.exists('aces.db') == False:    
+    if os.path.exists('aces.db') == False:
         acesdb = AceDB(data['dbusername'])
         # initalize sqldb object
         acesdb.setup()
-    
+
     # check for new rewards accounts to initialize if any changed
     acesdb = AceDB(data['dbusername'])
-    
+
     # get requested info for listener CURRENTLY HARDCODED FOR TESTING
     contract = str(uuid4())
     ts = int(time.time())
-    send_address = "DGExsNogZR7JFa2656ZFP9TMWJYJh5djzQ"
+    send_address = "DS2YQzkSCW1wbTjbfFGVPzmgUe1tNFQstN"
     fee = data['flat_fee']
     send_amount = (1+fee) * atomic
     receive_addr = "DGExsNogZR7JFa2656ZFP9TMWJYJh5djzQ"
     receive_amount = 1 * atomic
-    
-    
-    # get input from front end 
+
+
+    # get input from front end
     # currently will do manually until front end is built for testing
     new_contract = ((contract, ts, send_address, send_amount, receive_addr, receive_amount, fee),)
     acesdb.storeContracts(new_contract)
