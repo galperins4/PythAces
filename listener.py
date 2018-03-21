b46e586664e262aa2f4a0bc1b88d1fc409a35815
galperins4 <galperins4@gmail.com>
2018-03-20 21:28:10 -0500

    Update listener.py

--- listener.py
+++ listener.py
@@ -63,13 +63,9 @@ if __name__ == '__main__':
                         #check if contract matches vendor field
                         if c[0] == tx[5] and c[2] == tx[1] and A["service_acct"] == tx[2] and c[3] == tx[3]:
                             #we have a match - mark as processed and move to staging
-                            #store payment
-                            msg = "Thanks for using PythAces"
+                            #store payment and mark as processed
+                            msg = "Thanks for using PythAces - contract "+c[0]
                             acesdb.storePayment(c[0], c[4], c[5], msg)
-                            print("matched contract found")
-                            print("mark as processed")
-                        
-                            #mark as processed
                             acesdb.markAsProcessed(c[0])
                     #increment rows processed
                     start_row += tx_cnt
