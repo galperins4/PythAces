407ba5e85937c99d23185c56002e7bd931045ea3
galperins4 <galperins4@gmail.com>
2018-03-22 22:58:45 -0500

    Add files via upload

---
+++ contracts.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from uuid import uuid4
+
+class Contract:
+    def __init__(self):
+        self.contract = ()
+        self.uid = str(uuid4())
+        self.contract = self.contract + (self.uid,)
+        
+    def create(self, ts, saddr, samt, raddr, ramt, fee):
+        self.contract = self.contract + (ts, saddr, samt, raddr, ramt, fee)
+        return self.contract
