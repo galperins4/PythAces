c9f27cfee8d383542c17e1e7bc934edb48f7137f
galperins4 <galperins4@gmail.com>
2018-03-23 23:27:11 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, json, request
 from core.acedb import AceDB
 from core.pythaces import Pythaces
 from core.contracts import Contract
+from core.conversion import Conversion
 from pay import parse_config, get_network
 import time
 import os.path
@@ -11,32 +12,63 @@ app = Flask(__name__)
 
 @app.route("/prices")
 def prices():
-    return "Return Employee JSON data"
+    
+    try:
+        #pythaces class
+        conv = Conversion()
+        
+        conversion_rate = conv.conversion_rate()
+           
+        priceDict = {
+                "Address": A['service_acct'],
+                "Conversion Rate":  conversion_rate,
+                "Flat fee": data['flat_fee'],
+                "Percent fee": data['pct_fee']
+                }
+    
+    except:
+        print("Error")
+    
+
+    return jsonify(Capacity=priceDict)
 
 @app.route('/contracts', methods=['POST']) 
 def contracts():
     print(request.json)
+    
+    '''
+    c = Contract()
+    c.create(ts, send_address, send_amount, receive_addr, receive_amount, total_fee)
+    test = (c.contract,)
+    acesdb.storeContracts(test)
+    print("Contract Stored!")
+    '''
+    
     return json.dumps(request.json)
 
 @app.route("/capacity")
 def capacity():
-    #pythaces class
-    b = get_network(B, network, B['relay_ip'])
-    pythaces = Pythaces(b, atomic)
+    try:
+        #pythaces class
+        b = get_network(B, network, B['relay_ip'])
+        pythaces = Pythaces(b, atomic)
     
-    capacity = pythaces.service_capacity(B['service_acct'])
+        capacity = pythaces.service_capacity(B['service_acct'])
         
-    #reserved_capacity = 
-    contracts = acesdb.unprocessedContracts()
-    reserved = pythaces.reserve_capacity(contracts)
+        #reserved_capacity = 
+        contracts = acesdb.unprocessedContracts()
+        reserved = pythaces.reserve_capacity(contracts)
     
-    #available_capacity = 
-    available = pythaces.available_capacity()
+        #available_capacity = 
+        available = pythaces.available_capacity()
     
-    capDict = {
+        capDict = {
                 "Total Capacity": capacity,
                 "Reserved Capacity": reserved,
                 "Avaiable Capacity": available}
+        
+    except:
+        print("Unsuccessful")
         
     return jsonify(Capacity=capDict)
 
