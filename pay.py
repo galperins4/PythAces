13dd1bdb5b361d8564269de9f87ea6fed4e410fe
galperins4 <galperins4@gmail.com>
2018-03-25 23:46:18 -0500

    Update pay.py

--- pay.py
+++ pay.py
@@ -185,7 +185,7 @@ if __name__ == '__main__':
                     # get passphrases
                     pp, sp = get_passphrases(net)
                     #send transaction - TO DO - NEED TO ADD PEER CAPABILITIES
-                    transaction = park.transaction().create(i[2], str(i[3]), i[4], pp, sp)
+                    transaction = park.transactions().create(i[2], str(i[3]), i[4], pp, sp)
                     print(transaction)
                     #transaction = park.transactions().create('address', 'amount', 'vendor', 'secret', 'second secret')
                     
