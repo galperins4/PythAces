d710ef3170ee6381030794dad525f55ba09c9e7f
galperins4 <galperins4@gmail.com>
2018-03-26 23:34:49 -0500

    Add files via upload

--- core/contracts.py
+++ core/contracts.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 from uuid import uuid4
+from conversion import Conversion
+atomic = 100000000
+
 
 class Contract:
     def __init__(self):
@@ -11,3 +14,17 @@ class Contract:
     def create(self, ts, saddr, samt, raddr, ramt, fee):
         self.contract = self.contract + (ts, saddr, samt, raddr, ramt, fee)
         return self.contract
+       
+    def pricing(self,a,b,amt,fees):
+    	cnv = Conversion(a, b)
+        rate = cnv.conversion_rate()
+        
+        convert_amount = amt*rate*atomic
+        
+        f_fee = fees['flat_fee']*atomic
+        p_fee = fees['pct_fee']*convert_amount
+        total_fee = int((p_fee + f_fee))
+        send_amount = int((converted_amount)) + total_fee
+        
+        return send_amount, total_fee
+    	
