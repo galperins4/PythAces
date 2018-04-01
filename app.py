c98a0263e5c688254b7793f0cf2ad67a416a7183
galperins4 <galperins4@gmail.com>
2018-03-31 22:18:59 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,46 +1,68 @@
-from flask import Flask, jsonify, json, request
+from flask import Flask, jsonify, json, request, render_template, make_response
+from flask_cors import CORS
 from core.acedb import AceDB
 from core.pythaces import Pythaces
 from core.contracts import Contract
 from core.conversion import Conversion
 from pay import parse_config, get_network
+import requests
 import time
 import os.path
 
 atomic = 100000000
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 #main landing page
 def home():
-	
     pass
 
-@app.route("/coin", methods=['POST'])
-def coin():	
-	# get send/receive addresses and amouunt
-	print(request.json())
-	send = request.args.get("send", type=str)
-	recieve = request.args.get("receive", type=str)
-        amount = request.args.get("amount", type=float)
-        
-        
-        # do validations
-        
-        # calculate value
-        
-        #create contract
-        '''
-    c = Contract()
-    c.create(ts, send_address, send_amount, receive_addr, receive_amount, total_fee)
-    test = (c.contract,)
-    acesdb.storeContracts(test)
-    print("Contract Stored!")
-    '''
+@app.route("/<coin>", methods=['POST'])
+def crypto(coin):
+    # get send/receive addresses and amouunt
+    req_data = request.get_json()
+    send = req_data['send']
+    receive = req_data["receive"]
+    amount = req_data["amount"]*atomic
+    # do validations
+    c1, c1_msg = validate_addresses(coin,send,receive)
+    c2, c2_msg = validate_amount(coin,amount)
+
+    a_check = all([c1, c2])
+
+    if a_check == True:
     
+        # calculate value
+        f = {'flatFee': data['flat_fee'],
+             'pctFee': data['pct_fee']} 
+
+        c = Contract()
+        send_amount, total_fee = c.pricing(data['channel'], coin, amount, f)
+
+        ts = int(time.time())
+    
+        c.create(ts, send, send_amount, receive, amount, total_fee)
+        new_contract = (c.contract,)
+        acesdb.storeContracts(new_contract)
+
+        address = data['service_acct'] 
+        amt = send_amount / atomic
+        vendorfield = c.uid
+
+        qr_dict={"success":True,"address":address,"amount":amt, "vendorField":vendorfield, "receive":(amount/atomic)} 
         #return json to client (address, amount, vendorfield)
-        return json.dumps(request.json)
+        return jsonify(qr_dict)
+
+    else:
+        if c1:
+            msg = c2_msg
+        else:
+            msg = c1_msg
+
+        return jsonify(msg)
+
 
 #display all prices
 @app.route("/prices")
@@ -48,39 +70,53 @@ def prices():
     
     try:
     	
-    	conversion_rates = {} 
+        conversion_rates = {} 
         # get conversion rates
         for key in coin:
-        	cnv = Conversion(data['channel'], key)
-        	conversion_rates[key] = cnv.conversion_rate()
+            cnv = Conversion(data['channel'], key)
+            conversion_rates[key] = cnv.conversion_rate()
         # get fees	
         feeDict = {
-                "Flat fee": data['flat_fee'],
-                "Percent fee": data['pct_fee']
+                "flatFee": data['flat_fee'],
+                "percentFee": data['pct_fee']*100
                 }
                 
         priceDict = {
                 "prices": conversion_rates,
                 "fees": feeDict}
                 
-        return jsonify(Prices=priceDict)
+        return jsonify(priceDict)
     
-    except:	
-        error ={"Status":"Unsuccessful"}
+    except:
+        error ={"success":False, "msg":"Prices not available"}
         return jsonify(Error=error)
 
 #Change this to just send contracts
 @app.route('/contracts') 
 def contracts():
-    pass
+    filtered_contracts=[]
+    all_contracts = acesdb.contracts().fetchall()
+    for i in all_contracts:
+        if i[7] != "Expired":
+            tmp={"contract":i[0],
+                 "createdOnTimestamp":i[1],
+                 "sendingAddress":i[2],
+                 "sendingAmount":i[3]/atomic,
+                 "receivingAddress":i[4],
+                 "receivingAmount":i[5]/atomic,
+                 "totalFees":i[6]/atomic,
+                 "contractStatus":i[7],
+                 "processedOnTimestamp":i[8]}
+            filtered_contracts.append(tmp)
+
+    return jsonify(filtered_contracts)
 
 #get all capacity
 @app.route("/capacity")
 def capacity():
-    try:
-        service_availability = {}
-    contracts = acesdb.unprocessedContracts()
-    
+
+    service_availability = {}
+    contracts = acesdb.unprocessedContracts().fetchall()
     for key in coin:
         pythaces = Pythaces(fx_coins[key], atomic)
         # total capacity
@@ -90,14 +126,60 @@ def capacity():
         #available capacity
         available = pythaces.available_capacity()
         
-        service_availability[key] = {"Total Capacity": capacity,
-                                    "Reserved Capacity": reserved,
-                                    "Available Capcity": available}
+        service_availability[key] = {"totalCapacity": capacity,
+                                    "reservedCapacity": reserved,
+                                    "availableCapacity": available}
         
-        return jsonify(Capacity=service_availability)
+    return jsonify(service_availability)
+    '''
     except:
-    	error ={"Status":"Unsuccessful"}
+        error ={"success":False, "msg":"Capacity is not available"}
         return jsonify(Error=error)
+    '''
+
+def validate_amount(c,amount):
+    
+    # check against limit 
+    url = "http://"+data['channel_ip']+":5000/capacity"
+    r = requests.get(url)
+    avail_cap = r.json()[c]["availableCapacity"] - atomic
+
+    limit = 100*atomic
+    if amount <= limit:
+        if amount < avail_cap:
+            flag = True
+            m = amount
+        else:
+            flag = False
+            m = "amount is currently over capacity"
+    else:
+        flag = False
+        m = "amount is currently over capacity or limit" 
+
+    
+    msg = {"success":flag, "msg":m}
+    return flag, msg
+
+
+def validate_addresses(c, a_addr, b_addr):
+
+    a_len=len(a_addr)
+    b_len=len(b_addr)
+    a_check = data["service_acct"][0]
+    b_check = coin[c]["addr_start"]
+
+    # set response to invalid
+    flag = False
+    msg = {"success":flag, "msg":"one or both addresses invalid, please double check"}
+
+    # check that address is valid address length and network
+    all_check= all([a_len==34,a_addr[0]==a_check,b_len==34,b_addr[0]==b_check])
+    if all_check:
+    # both addresses are good, flip flag and message
+        flag = True
+        msg = {"success": flag}
+
+    return flag, msg
 
 if __name__ == "__main__":
     # get config data
@@ -118,4 +200,4 @@ if __name__ == "__main__":
     for key in coin:
         fx_coins[key] = get_network(key, network, coin[key]['relay_ip'])
       
-    app.run(host = data['channel_ip'], threaded=True)
+    app.run(host=data['channel_ip'],threaded=True)
