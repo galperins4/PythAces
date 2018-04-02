7518d22ed9ca3109bf9e01d6a21dd3afd19c2dad
galperins4 <galperins4@gmail.com>
2018-04-01 21:09:22 -0500

    Add files via upload

--- core/acedb.py
+++ core/acedb.py
@@ -1,5 +1,8 @@
 import sqlite3
 import time
+import threading
+
+lock = threading.Lock()
 
 class AceDB:
     def __init__(self, u):
@@ -10,10 +13,22 @@ class AceDB:
         return self.connection.commit()
 
     def execute(self, query, args=[]):
-        return self.cursor.execute(query, args)
+        try:
+            lock.acquire(True)
+            res = self.cursor.execute(query, args)
+        finally:
+            lock.release()
+            
+        return res
 
     def executemany(self, query, args):
-        return self.cursor.executemany(query, args)
+        try:
+            lock.acquire(True)
+            res = self.cursor.executemany(query, args)
+        finally:
+            lock.release()
+        
+        return res
 
     def fetchone(self):
         return self.cursor.fetchone()
@@ -97,6 +112,9 @@ class AceDB:
         
     def contracts(self):
         return self.cursor.execute("SELECT * FROM contracts")
+    
+    def singleContract(self, id):
+        return self.cursor.execute("SELECT * FROM contracts WHERE contract = '{id}'")
     
     def transactions(self):
         return self.cursor.execute("SELECT * FROM transactions")
