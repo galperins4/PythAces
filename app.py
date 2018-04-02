b4245ad9ff092eab63d08c4f969aa4edb4578ddb
galperins4 <galperins4@gmail.com>
2018-04-01 21:46:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -117,13 +117,7 @@ def contracts():
 
 @app.route('/api/contracts/<id>')
 def get_contract(id):
-    new_id = ""
-    for i in id:
-        new_id+=i
-    
-    new_id = str(new_id)
-    print(type(new_id))
-    contract_id = acesdb.singleContract(new_id).fetchall()
+    contract_id = acesdb.singleContract(id).fetchall()
     jsoned = contract_to_json(contract_id)
     return jsonify(jsoned)
     
