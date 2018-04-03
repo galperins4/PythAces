c6cb614e7ed641fce9b9a2b7b83745c0ed146c85
galperins4 <galperins4@gmail.com>
2018-04-02 22:47:32 -0400

    Update pythaces.py

--- core/pythaces.py
+++ core/pythaces.py
@@ -13,10 +13,11 @@ class Pythaces:
     def reserve_capacity(self, contracts, letter):
         s = 0
         # loop through contracts
-        for i in contracts:
-            # check for starting letter of send to address for network
-            if i[4][0] == letter:
-                s+=i[5]
+        if contracts:
+            for i in contracts:
+                # check for starting letter of send to address for network
+                if i[4][0] == letter:
+                    s+=i[5]
             
         self.reserve_capacity = s
         
