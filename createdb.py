0434f6d8671557ac4e31949217088333fd7386f0
galperins4 <galperins4@gmail.com>
2018-04-04 15:02:20 -0400

    Update createdb.py

--- createdb.py
+++ createdb.py
@@ -2,12 +2,11 @@ from pay import parse_config
 from core.acedb import AceDB
 import os.path
 
-if name 
+if __name__ == "__main__":
 
 # get config data
     data, network, coin = parse_config()
     
-    #listener listens from cryptoA
     # check to see if ark.db exists, if not initialize db, etc
     if os.path.exists('aces.db') == False:    
         acesdb = AceDB(data['dbusername'])
