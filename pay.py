6e54252422776acfeaa0ddff7d02198e0021cbba
galperins4 <galperins4@gmail.com>
2018-04-05 19:38:32 -0400

    Update pay.py

--- pay.py
+++ pay.py
@@ -4,41 +4,9 @@ from core.acedb import AceDB
 from park.park import Park
 from liskbuilder.transaction import TransactionBuilder
 # import random
+from core.util import parse_config, get_network, get_passphrases
 import time
 import json
-
-def get_network(d, n, ip="localhost"):
-
-    return Park(
-        ip,
-        n[d]['port'],
-        n[d]['nethash'],
-        n[d]['version']
-    )
-
-def parse_config():
-    """
-    Parse the config.json file and return the result.
-    """
-        
-    with open('config/networks.json') as network_file:
-        network = json.load(network_file)
-        
-    with open('config/coin.json') as coin_file:
-        coin = json.load(coin_file)
-        
-    return network, coin
-
-def get_passphrases(c):
-    # Get the passphrase
-    passphrase = coin[c]['service_account_passphrase']
-    
-    # Get the second passphrase
-    secondphrase = coin[c]['service_account_secondphrase']
-    if secondphrase == 'None':
-        secondphrase = None
-
-    return passphrase, secondphrase
 
 '''
 def get_peers(park):
